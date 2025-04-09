20c3cdfc845ca9c0c5d60617467aff19b4c9b484
Samuel Egli <samuelegligarcia@outlook.de>
2025-04-09 16:09:38 +0200

    Script wurde geschrieben, dass einen Dad Joke von einer Rest API holt und auf dem Homebildschirm aufzeigt.

---
+++ Public/script.js
@@ -0,0 +1,25 @@
+const jokediv = document.getElementById('joke');
+const newJoke = document.getElementById('newJoke');
+
+newJoke.addEventListener('click', () => {
+    getJoke()
+})
+
+function updateJokediv(newText) {
+    jokediv.innerText = newText;
+}
+
+async function getJoke() {
+    try {
+        const response = await fetch("https://icanhazdadjoke.com/", {headers: {
+          'Accept': 'application/json'
+        }});
+        if (!response.ok) {
+          console.error('Antwort war nicht ok');
+        }
+        const data = await response.json();
+        updateJokediv(data.joke)
+      } catch (error) {
+        console.error('Fehler beim Laden:', error)
+      }
+}
